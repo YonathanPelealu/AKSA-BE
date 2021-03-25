3c2bc508a186e9f2367184a8da9ea92586d66e25
andika.pelealu90 <andika.pelealu90@gmail.com>
2021-03-25 14:06:16 +0700

    testing eslint semi

---
+++ migrations/20210325064721-create-author.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Authors', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      Name: {
+        type: Sequelize.STRING
+      },
+      Age: {
+        type: Sequelize.INTEGER
+      },
+      Description: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Authors');
+  }
+};
---
+++ migrations/20210325065014-create-book.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      authorId: {
+        type: Sequelize.UUID
+      },
+      Title: {
+        type: Sequelize.STRING
+      },
+      ReleaseYear: {
+        type: Sequelize.DATE
+      },
+      Description: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Books');
+  }
+};
